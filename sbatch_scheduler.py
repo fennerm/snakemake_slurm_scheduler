1676324131666d94e8b0c3343bb1514b8e9fab0d
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-03-08 15:55:57 -0800

    sbatch command now sends the job id to snakemake

--- sbatch_scheduler.py
+++ sbatch_scheduler.py
@@ -1,12 +1,22 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 """
 Snakemake SLURM scheduler script
 """
+from __future__ import print_function
 
 import os
+from subprocess import (
+    PIPE,
+    Popen,
+    STDOUT,
+)
 import sys
 
 from snakemake.utils import read_job_properties
+
+
+def errprint(x):
+    print(x, file=sys.stderr)
 
 
 def main(args):
@@ -26,20 +36,21 @@ def main(args):
         cmd.append("--dependency")
         cmd.append("afterok:" + ",".join(dependencies))
 
-    cmd = cmd + [
-        "--tasks-per-node", threads, "--mem", mem, "--job-name", name, jobscript
-    ]
-    cmd = ' '.join(cmd)
+    cmd += ["--tasks-per-node", threads, "--mem", mem, "--job-name", name,
+            jobscript]
 
     # Submit job
-    print(
-        '\n'.join([
-            "Submit job with parameters:", "name : " + name,
-            "threads : " + threads, "mem : " + mem
-        ]),
-        file=sys.stderr)
-    print("Full command: " + cmd, file=sys.stderr)
-    os.system(cmd)
+    errprint('Submit job with parameters:')
+    errprint('name: ' + name)
+    errprint('threads: ' + threads)
+    errprint('mem(mb): ' + mem)
+    errprint('sbatch command: ' + cmd)
+
+    sbatch_stdout = Popen(cmd, stdout=PIPE, stderr=STDOUT,
+                          shell=True).communicate()[0]
+
+    # Snakemake expects the job's id to be sent to stdout by the scheduler
+    print("%i" % int(sbatch_stdout.split()[-1]), file=sys.stdout)
 
 
 if __name__ == "__main__":
