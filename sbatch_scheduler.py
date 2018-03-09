fa248b182a495a5a05dce1e67d51539bc2626155
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-03-08 17:24:45 -0800

    Small fixes

--- sbatch_scheduler.py
+++ sbatch_scheduler.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 Snakemake SLURM scheduler script
 """
 from __future__ import print_function
 
-import os
 from subprocess import (
     PIPE,
     Popen,
@@ -25,7 +24,6 @@ def main(args):
     jobscript = str(args[-1])
     job_properties = read_job_properties(jobscript)
     job_resources = job_properties["resources"]
-    name = str(job_properties['rule'])
     threads = str(job_properties.get("threads", "1"))
     mem = str(job_resources.get("mem_mb", "10000"))
 
@@ -36,21 +34,21 @@ def main(args):
         cmd.append("--dependency")
         cmd.append("afterok:" + ",".join(dependencies))
 
-    cmd += ["--tasks-per-node", threads, "--mem", mem, "--job-name", name,
-            jobscript]
+    cmd += ["--tasks-per-node", threads, "--mem", mem,
+            "-p", "short", jobscript]
 
     # Submit job
     errprint('Submit job with parameters:')
-    errprint('name: ' + name)
     errprint('threads: ' + threads)
     errprint('mem(mb): ' + mem)
-    errprint('sbatch command: ' + cmd)
+    errprint('sbatch command: ' + ' '.join(cmd))
 
     sbatch_stdout = Popen(cmd, stdout=PIPE, stderr=STDOUT,
                           shell=True).communicate()[0]
 
     # Snakemake expects the job's id to be sent to stdout by the scheduler
     print("%i" % int(sbatch_stdout.split()[-1]), file=sys.stdout)
+    sys.exit()
 
 
 if __name__ == "__main__":
